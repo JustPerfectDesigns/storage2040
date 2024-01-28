6827ea1e533620cfe4b173d644e1b0a7f84d00b4
JustPerfectDesigns <gabriel.jpdesigns@gmail.com>
2024-01-28 14:19:09 +0100

    Cart Commit New

--- components/ShoppingCartModal.jsx
+++ components/ShoppingCartModal.jsx
@@ -71,7 +71,7 @@ export default function ShoppingCartModal() {
 
 											<div className="ml-4 flex flex-1 flex-col">
 												<div>
-													<div className="flex justify-between text-base font-medium text-gray-900">
+													<div className="flex justify-between text-sm font-semibold md:text-base md:font-medium text-gray-900">
 														<h3>{entry.name}</h3>
 														<p className="ml-4">${entry.price}</p>
 													</div>
